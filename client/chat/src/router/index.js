a4417d42ab505beae1b8bd701d6164a912249a32
MiguelCaAr <castroarredondo.hector@gmail.com>
2024-04-12 23:44:46 -0700

    - Tailwind intalled - Folders and files not required have been deleted - Router changed

--- client/chat/src/router/index.js
+++ client/chat/src/router/index.js
@@ -1,20 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "login",
+      component: () => import("../login/views/LoginView.vue"),
     },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
